7a370042bb12a3b72d40d62b68e7244ca1c1ebac
David López García <dlopez@ugr.es>
2024-04-12 12:06:53 +0200

    Bug fixed in sliding filter analysis. The filesLocation folder is removed when a new analysis is computed. This is required in order to prevent errors if the user repeat the analysis with less frequency steps.

--- src/functions/sf/mvpalab_preparesf.m
+++ src/functions/sf/mvpalab_preparesf.m
@@ -7,6 +7,14 @@ cfg.sf.savefolder = [cfg.location filesep 'results' filesep 'diffMaps' filesep];
 cfg = mvpalab_genfreqvec(cfg);  % Generate cutoff frequencies.
 cfg = mvpalab_sfmetrics(cfg);   % Update performace metrics.
 
+% If the filesLocation folder exists, remove it for the new analysis. This
+% is required in order to prenvent errors if the user repeat the analysis
+% with less frequency steps.
+
+if exist(cfg.sf.filesLocation,'dir')
+    rmdir(cfg.sf.filesLocation,'s');
+end
+
 mvpalab_mkdir(cfg.sf.filesLocation); % Create data folder.
 mvpalab_mkdir(cfg.sf.savefolder);    % Create result folder.
 mvpalab_mkdir([cfg.sf.savefolder 'other' filesep]);
