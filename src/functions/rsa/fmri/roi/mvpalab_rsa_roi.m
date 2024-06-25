0a759f02605698291811c358e0ef976f4c30a105
David López García <dlopez@ugr.es>
2024-06-25 10:56:23 +0200

    The fMRI RDMs for each ROI are now saved in different folders.

--- src/functions/rsa/fmri/roi/mvpalab_rsa_roi.m
+++ src/functions/rsa/fmri/roi/mvpalab_rsa_roi.m
@@ -11,49 +11,50 @@ dist = cfg.rsa.distance;
 
 theo = mvpalab_gentrdms(cfg,cfg.rsa.tmodels);
 vtheo = mvpalab_vectorizerdm(cfg,theo);
-
-%% Subjects loop:
-%  Iterate along subjects:
-
-for sub = 1 : length(volumes)
     
-    subject_rois = masks{sub};
-    subject_rois_names = fieldnames(subject_rois);
-    subject_betas = volumes{sub};
+%% ROIs loop:
+%  Iterate along brain regions:
+for roi = 1 : length(cfg.rsa.roi)
+    %% Subjects loop:
+    %  Iterate along subjects:
+
+    for sub = 1 : length(volumes)
     
-    %% ROIs loop:
-    %  Iterate along brain regions:
-    for roi = 1 : length(subject_rois_names)
+        subject_rois = masks{sub};
+        subject_rois_names = fieldnames(subject_rois);
+        subject_betas = volumes{sub};
+    
+
         
         subject_roi_name = subject_rois_names{roi};
         subject_roi = subject_rois.(subject_roi_name);
         
         %% Masked data:
-        masked{sub,roi} = mvpalab_maskbetas(subject_roi,subject_betas);
+        masked{sub,1} = mvpalab_maskbetas(subject_roi,subject_betas);
         
         %% Prepare data: [betas x voxels]
-        masked{sub,roi} = mvpalab_combineruns(cfg,masked{sub,roi});
+        masked{sub,1} = mvpalab_combineruns(cfg,masked{sub});
         
         %% Extract RDM for each roi and subject:
         %  This function returns 2D matrices containing the RDMs according
         %  to the following structure:
         %  rdm - [n_conditions (per run) x voxels]
         
-        rdm{sub,roi} = mvpalab_computerdm(cfg,masked{sub,roi});
+        rdm{sub,1} = mvpalab_computerdm(cfg,masked{sub});
         
         %% Merge conditions if needed:
         %  Combine conditions of different runs in a global condition if
         %  needed.
         %  rdm - [n_conditions x voxels]
         
-        rdm{sub,roi} = mvpalab_mergerunsrdm(cfg,subject_betas,rdm{sub,roi});
+        rdm{sub,1} = mvpalab_mergerunsrdm(cfg,subject_betas,rdm{sub});
         
         %% Vectorize RDMs:
         %  Both theoretical and empirical DRMs are vectorized in order to
         %  compute the Representational Similarity Analysis.
         %  vrdms - [1 x vectorized]
         
-        vrdms{sub,roi} = mvpalab_vectorizerdm(cfg,rdm{sub,roi});
+        vrdms{sub,1} = mvpalab_vectorizerdm(cfg,rdm{sub});
         
         %% Compute the Representational similarity analysis:
         %  Empirical and theoretical models are correlated to obtain the time
@@ -61,11 +62,14 @@ for sub = 1 : length(volumes)
         
         
     end
+    
+    %% Save RSA data
+    res.(mode).(dist).(subject_roi_name).rdms = rdm;
+    res.(mode).(dist).(subject_roi_name).theo = theo;
+    
 end
 
-%% Save RSA data
-res.(mode).(dist).rdms = rdm;
-res.(mode).(dist).theo = theo;
+
 
 mvpalab_save(cfg,res,'res');
 
